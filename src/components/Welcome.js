4daa91f8829681b566e70d70aa0c50c1e4302db8
Adarsh Rangare <akrangare@gmail.com>
2023-12-24 18:29:12 +0530

    Update Welcome.js

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -6,7 +6,7 @@ export default function Welcome({name}){
   return(
     <div>
       <h1>Hey {name}!</h1>
-      <h2>Welcome to Newton School</h2>    
+      <h2>Welcome to Newton School.</h2>    
     </div>
   )
 }
